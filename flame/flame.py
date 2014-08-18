64bbffdd9072799e3b1873cb56f4bc8edf225f9f
Kyle Cesare <kcesare@gmail.com>
2014-08-17 22:20:09 -0700

    Find hotspot clusters and and improve drawing.

--- flame/flame.py
+++ flame/flame.py
@@ -1,11 +1,17 @@
 import pygame
-import numpy as np
-import scipy.signal
 import osgeo.gdal
 
+import numpy as np
+import scipy.signal
+import sklearn.cluster
+
+# The rate at which to step through the simulation
 TIME_STEP = 0.05
 
 class Fire(object):
+    # Minimum normalized value for a point to be considered a hotspot
+    HOTSPOT_MIN = 0.6
+
     def __init__(self, toa_file, fli_file):
         self.time_of_arrival = osgeo.gdal.Open(toa_file).ReadAsArray()
         self.fire_intensity = osgeo.gdal.Open(fli_file).ReadAsArray()
@@ -22,16 +28,40 @@ class Fire(object):
         # not entirely surrounded
         frontier = self.fire_progression > 0
         frontier = scipy.signal.convolve2d(frontier, [[1, 1, 1], [1, 0, 1], [1, 1, 1]], mode='same')
-        frontier = np.logical_and(frontier < 8, frontier > 0)
-        return np.asarray(frontier.nonzero())
+        frontier = np.logical_and(frontier < 8, frontier > 0).nonzero()
+
+        return frontier
+
+
+    def extract_clusters(self):
+        # Copy the frontier to a full matrix so we can compare it with the
+        # hotspots
+        frontier_mask = np.zeros_like(self.fire_progression)
+        frontier_mask[self.frontier] = 1
+
+        hotspots = np.where(self.fire_progression > self.HOTSPOT_MIN,
+                np.ones_like(self.fire_progression),
+                np.zeros_like(self.fire_progression))
+        hotspots = np.logical_and(hotspots, frontier_mask)
+        hotspots = np.asarray(np.nonzero(hotspots)).T
+
+        if hotspots.shape[0] > 2:
+            kmeans = sklearn.cluster.KMeans(n_clusters=2)
+            kmeans.fit(hotspots)
+
+            return kmeans.cluster_centers_
+
+        return np.empty((0, 2))
 
     def update(self, simulation_time):
         self.fire_progression = np.where(self.time_of_arrival <= simulation_time,
-                self.fire_intensity * 255, np.zeros_like(self.fire_intensity))
+                self.fire_intensity, np.zeros_like(self.fire_intensity))
+        self.frontier = self.extract_frontier()
+        self.clusters = self.extract_clusters()
 
     def draw(self, screen):
         surface = pygame.Surface(self.fire_progression.shape)
-        pygame.surfarray.blit_array(surface, self.fire_progression.astype(np.uint32))
+        pygame.surfarray.blit_array(surface, (self.fire_progression * 255).astype(np.uint32))
 
         screen.blit(surface, (0, 0))
 
@@ -57,7 +87,7 @@ def run():
         Vehicle()
     ]
 
-    screen = pygame.display.set_mode(fire.shape())
+    screen = pygame.display.set_mode(fire.shape(), pygame.HWSURFACE | pygame.DOUBLEBUF)
     clock = pygame.time.Clock()
 
     simulation_time = 0
@@ -68,9 +98,20 @@ def run():
             entity.update(simulation_time)
             entity.draw(screen)
 
-        frontier = fire.extract_frontier()
+        # Draw frontier on alpha surface and blit it to the screen
+        frontier_surf = pygame.Surface(fire.shape(), pygame.SRCALPHA)
+        frontier = np.asarray(fire.frontier)
         for (x, y) in frontier.T:
-            pygame.draw.rect(screen, (0, 255, 0), (x, y, 2, 2))
+            frontier_surf.set_at((x, y), (0, 255, 0, 20))
+        screen.blit(frontier_surf, (0, 0))
+
+        # Draw clusters on alpha surface and blit it to the screen
+        cluster_surf = pygame.Surface(fire.shape(), pygame.SRCALPHA)
+        clusters = fire.clusters.astype(np.uint)
+        if clusters.any():
+            for (x, y) in clusters:
+                pygame.draw.circle(cluster_surf, (255, 0, 0, 128), (x, y), 6)
+        screen.blit(cluster_surf, (0, 0))
 
         simulation_time += TIME_STEP
 
